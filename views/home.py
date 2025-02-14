8221451053a2127d81d9cb496ae3db439bc72c70
nurainundaulayy <nurainundaulay@outlook.co.id>
2025-02-14 16:15:58 +0700

    home.py

--- views/home.py
+++ views/home.py
@@ -107,76 +107,76 @@ with st.container():
 
         
     
-# --- CHATBOT DESCRIPTION ---
-with st.container():
-    st.write("---")
-    left_column, right_column = st.columns((1,2), vertical_alignment="center")
-    with left_column:
-        st_lottie(lottie_animation_personalchat, height=300, key="chatbot")
-    with right_column:
-        st.header("Pemantauan 1000 HPK secara Personal")
-        st.html("<p>Mari memelihara 1000 Hari Pertama Kehidupan (HPK) bersama! NutriCare 1000 Chatbot siap memantau kondisi gizi ibu dan bayi, memberikan saran dan informasi, serta kemudahan konsultasi. Bersama NutriCare 1000, kita wujudkan generasi bebas stunting sejak awal kehidupan!</p>")
-        st.markdown(
-        '''
-        <style>
-            .hover-link {
-                background-color: #0e1117;
-                color: white;
-                padding: 10px 20px;
-                border: none;
-                border-radius: 5px;
-                outline: 1px solid #41444c;
-                text-decoration: none;
-                transition: background-color 0.3s ease, transform 0.3s ease;
-            }
-            .hover-link:hover {
-                background-color: #333;
-                transform: scale(1.05);
-            }
-        </style>
-        <a class="hover-link" href="https://t.me/nutricare1000hpk_bot" target="_blank">
-            Buka Chatbot
-        </a>
-        ''',
-        unsafe_allow_html=True
-        )
+# # --- CHATBOT DESCRIPTION ---
+# with st.container():
+#     st.write("---")
+#     left_column, right_column = st.columns((1,2), vertical_alignment="center")
+#     with left_column:
+#         st_lottie(lottie_animation_personalchat, height=300, key="chatbot")
+#     with right_column:
+#         st.header("Pemantauan 1000 HPK secara Personal")
+#         st.html("<p>Mari memelihara 1000 Hari Pertama Kehidupan (HPK) bersama! NutriCare 1000 Chatbot siap memantau kondisi gizi ibu dan bayi, memberikan saran dan informasi, serta kemudahan konsultasi. Bersama NutriCare 1000, kita wujudkan generasi bebas stunting sejak awal kehidupan!</p>")
+#         st.markdown(
+#         '''
+#         <style>
+#             .hover-link {
+#                 background-color: #0e1117;
+#                 color: white;
+#                 padding: 10px 20px;
+#                 border: none;
+#                 border-radius: 5px;
+#                 outline: 1px solid #41444c;
+#                 text-decoration: none;
+#                 transition: background-color 0.3s ease, transform 0.3s ease;
+#             }
+#             .hover-link:hover {
+#                 background-color: #333;
+#                 transform: scale(1.05);
+#             }
+#         </style>
+#         <a class="hover-link" href="https://t.me/nutricare1000hpk_bot" target="_blank">
+#             Buka Chatbot
+#         </a>
+#         ''',
+#         unsafe_allow_html=True
+#         )
         
 
 
 
-# --- DASHBOARD DESCRIPTION ---
-with st.container():
-    st.write("---")
-    left_column, right_column = st.columns((2,1), vertical_alignment="center")
-    with left_column:
-        st.header("NutriCare 1000 Dashboard Monitoring")
-        st.html("<p>Dashboard ini menyajikan visualisasi data agregat pengguna untuk memantau demografi ibu dan anak, kondisi kesehatan ibu hamil, dan tumbuh kembang anak di masa1000 Hari Pertama Kehidupan (HPK). Informasi dari dashboard ini dapat dijadikan dasar dalam perumusan kebijakan pencegahan stunting berbasis data. Yuk pantau kondisinya!</p>")
-        #st.button("Buka Dashboard")
-        st.markdown(
-        '''
-        <style>
-            .hover-link {
-                background-color: #0e1117;
-                color: white;
-                padding: 10px 20px;
-                border: none;
-                border-radius: 5px;
-                outline: 1px solid #41444c;
-                text-decoration: none;
-                transition: background-color 0.3s ease, transform 0.3s ease;
-            }
-            .hover-link:hover {
-                background-color: #333;
-                transform: scale(1.05);
-            }
-        </style>
-        <a class="hover-link" href="https://nutricare-1000-by-nutriteam.streamlit.app/dashboard" target="_blank">
-            Buka Dashboard
-        </a>
-        ''',
-        unsafe_allow_html=True
-        )
-    with right_column:
-        st_lottie(lottie_animation_dashboard, height=300, key="dashboard-animation")
+# # --- DASHBOARD DESCRIPTION ---
+# with st.container():
+#     st.write("---")
+#     left_column, right_column = st.columns((2,1), vertical_alignment="center")
+#     with left_column:
+#         st.header("NutriCare 1000 Dashboard Monitoring")
+#         st.html("<p>Dashboard ini menyajikan visualisasi data agregat pengguna untuk memantau demografi ibu dan anak, kondisi kesehatan ibu hamil, dan tumbuh kembang anak di masa1000 Hari Pertama Kehidupan (HPK). Informasi dari dashboard ini dapat dijadikan dasar dalam perumusan kebijakan pencegahan stunting berbasis data. Yuk pantau kondisinya!</p>")
+#         #st.button("Buka Dashboard")
+#         st.markdown(
+#         '''
+#         <style>
+#             .hover-link {
+#                 background-color: #0e1117;
+#                 color: white;
+#                 padding: 10px 20px;
+#                 border: none;
+#                 border-radius: 5px;
+#                 outline: 1px solid #41444c;
+#                 text-decoration: none;
+#                 transition: background-color 0.3s ease, transform 0.3s ease;
+#             }
+#             .hover-link:hover {
+#                 background-color: #333;
+#                 transform: scale(1.05);
+#             }
+#         </style>
+#         <a class="hover-link" href="https://nutricare-1000-by-nutriteam.streamlit.app/dashboard" target="_blank">
+#             Buka Dashboard
+#         </a>
+#         ''',
+#         unsafe_allow_html=True
+#         )
+#     with right_column:
+#         st_lottie(lottie_animation_dashboard, height=300, key="dashboard-animation")
 
 
